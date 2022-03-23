7a414c57e50d73cbeda13ba6e25d2fd3844967db
MulleMiel <hungrierlion@gmail.com>
2022-03-23 22:35:18 +0100

    setupProxy with changeUrl param to true

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,13 @@
-const proxy = require('http-proxy-middleware').createProxyMiddleware;
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const dotenv = require('dotenv');
+dotenv.config();
 
-module.exports = function (app) {
-    app.use(proxy(`/auth/**`, { target: 'http://localhost:4000' }));
+module.exports = function(app) {
+  app.use(
+    '/auth/**',
+    createProxyMiddleware({
+      target: `http://localhost:${process.env.BACKEND_PORT}`,
+      changeOrigin: true,
+    })
+  );
 };
